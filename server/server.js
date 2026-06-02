622126460dda0a90c9cbc929dd729b2cead11c71
SarangMusale23 <work.sarangmusale@gmail.com>
2026-06-02 18:45:28 +0530

    Fix CORS for Vercel deployments

--- server/server.js
+++ server/server.js
@@ -14,15 +14,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://fitness-tracker-jxn2cq4kk-sarangmusales-projects.vercel.app",
-    ],
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
-);//line A
+);
 app.use(express.json()); //line B
 
 app.get("/", (req, res) => {
